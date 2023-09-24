f1c2f41022d561cd8beb167f41b4f2b2cbc5cc95
cobocho <rlaalsrb1111@naver.com>
2023-09-25 04:14:12 +0900

    Refactor: `TargetNumber`의 `number` 속성명 `value`로 변경

--- src/domain/TargetNumber.js
+++ src/domain/TargetNumber.js
@@ -2,45 +2,45 @@ const { isOutOfRange } = require('../utils/validator');
 const { ERROR_MESSAGE } = require('../constants/error');
 
 class TargetNumber {
-  static MIN = 1;
+	static MIN = 1;
 
-  static MAX = 9;
+	static MAX = 9;
 
-  static #TARGET_NUMBERS = Object.fromEntries(
-    Array.from({ length: TargetNumber.MAX }, (_, i) => [i + 1, new TargetNumber(i + 1)]),
-  );
+	static #TARGET_NUMBERS = Object.fromEntries(
+		Array.from({ length: TargetNumber.MAX }, (_, i) => [i + 1, new TargetNumber(i + 1)])
+	);
 
-  #number;
+	#value;
 
-  constructor(number) {
-    this.#validate(number);
-    this.#number = number;
-  }
+	constructor(value) {
+		this.#validate(value);
+		this.#value = value;
+	}
 
-  static valueOf(number) {
-    const targetNumber = TargetNumber.#TARGET_NUMBERS[number];
-    if (!targetNumber) {
-      throw new Error(ERROR_MESSAGE.MISSING_INSTANCE);
-    }
-    return targetNumber;
-  }
+	static valueOf(value) {
+		const targetNumber = TargetNumber.#TARGET_NUMBERS[value];
+		if (!targetNumber) {
+			throw new Error(ERROR_MESSAGE.MISSING_INSTANCE);
+		}
+		return targetNumber;
+	}
 
-  #validate(number) {
-    if (typeof number !== 'number') {
-      throw new Error(ERROR_MESSAGE.NOT_NUMBER('Target Number'));
-    }
-    const min = TargetNumber.MIN;
-    const max = TargetNumber.MAX;
-    if (isOutOfRange(number, min, max)) {
-      throw new Error(ERROR_MESSAGE.OUT_OF_RANGE({ target: 'Target Number', min, max }));
-    }
-  }
+	#validate(value) {
+		if (typeof value !== 'number') {
+			throw new Error(ERROR_MESSAGE.NOT_NUMBER('Target Number'));
+		}
+		const min = TargetNumber.MIN;
+		const max = TargetNumber.MAX;
+		if (isOutOfRange(value, min, max)) {
+			throw new Error(ERROR_MESSAGE.OUT_OF_RANGE({ target: 'Target Number', min, max }));
+		}
+	}
 
-  get number() {
-    return this.#number;
-  }
+	get value() {
+		return this.#value;
+	}
 }
 
 module.exports = {
-  TargetNumber,
+	TargetNumber,
 };
