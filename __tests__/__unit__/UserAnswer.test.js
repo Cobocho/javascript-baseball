18193dfa8dd31b153cf0882899584edce2af1303
cobocho <rlaalsrb1111@naver.com>
2023-09-25 17:17:25 +0900

    Test: `computeResult` 관련 테스트코드 작성

--- __tests__/__unit__/UserAnswer.test.js
+++ __tests__/__unit__/UserAnswer.test.js
@@ -1,5 +1,5 @@
 const { Random } = require('@woowacourse/mission-utils');
-const { UserAnswer, TargetNumber } = require('../../src/domain');
+const { UserAnswer, TargetNumber, Answer } = require('../../src/domain');
 const { ERROR_MESSAGE } = require('../../src/constants/error');
 
 describe('유저 정답 테스트', () => {
@@ -12,6 +12,34 @@ describe('유저 정답 테스트', () => {
 			userAnswer.answer.numbers.forEach((number, idx) => {
 				expect(number).toEqual(TargetNumber.valueOf(input[idx]));
 			});
+		}
+	);
+
+	it.each([
+		{ numbers: [1, 2, 3], strike: 3, ball: 0 },
+		{ numbers: [2, 3, 1], strike: 0, ball: 3 },
+		{ numbers: [1, 3, 5], strike: 1, ball: 1 },
+		{ numbers: [4, 5, 6], strike: 0, ball: 0 },
+	])(
+		'computeResult 메서드는 strike와 ball을 계산한다. (유저 정답: [1, 2, 3] 정답: $numbers strike: $strike ball: $ball)',
+		({ numbers, strike, ball }) => {
+			const answer = new Answer(numbers);
+			const userAnswer = new UserAnswer([1, 2, 3]);
+			userAnswer.computeResult(answer);
+
+			expect(userAnswer.strike).toBe(strike);
+			expect(userAnswer.ball).toBe(ball);
+		}
+	);
+
+	it.each([{ answer: [1, 2, 3] }, { answer: '[2, 3, 1]' }, { answer: undefined }, { answer: 1 }])(
+		'computeResult 메서드는 인자가 Answer 인스턴스가 아닐 시 에러가 발생한다.',
+		({ answer }) => {
+			const userAnswer = new UserAnswer([1, 2, 3]);
+
+			expect(() => {
+				userAnswer.computeResult(answer);
+			}).toThrow(ERROR_MESSAGE.USER_ANSWER.INVALID_ANSWER);
 		}
 	);
 });
